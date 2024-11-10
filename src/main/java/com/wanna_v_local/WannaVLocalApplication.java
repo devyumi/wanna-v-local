81995471119e8895665592c03475879f0f7c88ac
devyumi <yyumiwing@naver.com>
2024-11-10 13:39:26 +0900

    chore: 프로젝트 초기 구성

---
+++ src/main/java/com/wanna_v_local/WannaVLocalApplication.java
@@ -0,0 +1,13 @@
+package com.wanna_v_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannaVLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannaVLocalApplication.class, args);
+	}
+
+}
