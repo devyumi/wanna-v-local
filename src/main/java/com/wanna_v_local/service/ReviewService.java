41286920fd832a2b5ec32cb0de3c30607c95bb60
devyumi <yyumiwing@naver.com>
2024-11-12 11:06:50 +0900

    feat: 관리자 리뷰 관리 - 상세 조회 기능 추가

--- src/main/java/com/wanna_v_local/service/ReviewService.java
+++ src/main/java/com/wanna_v_local/service/ReviewService.java
@@ -1,14 +1,10 @@
 package com.wanna_v_local.service;
 
 import com.wanna_v_local.domain.Review;
-import com.wanna_v_local.dto.request.ReviewRequestDTO;
-import com.wanna_v_local.dto.response.ReviewResponseDTO;
+import com.wanna_v_local.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import repository.ReviewRepository;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,18 +12,14 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
+    /**
+     * 리뷰 상세 조회
+     *
+     * @param reviewId
+     * @return
+     */
     @Transactional(readOnly = true)
-    public ReviewResponseDTO findReviews(ReviewRequestDTO reviewRequestDTO) {
-        return ReviewResponseDTO.builder()
-                .reviewRequestDto(reviewRequestDTO)
-                .reviews(reviewRepository.findAll())
-                .total(reviewRepository.count())
-                .build();
-    }
-
-    @Transactional
-    public void updateReviewActive(Long id) {
-        Optional<Review> review = reviewRepository.findById(id);
-        reviewRepository.save(review.get());
+    public Review findReview(Long reviewId) {
+        return reviewRepository.findById(reviewId).orElseThrow(() -> new IllegalArgumentException("잘못된 id 값입니다."));
     }
 }
