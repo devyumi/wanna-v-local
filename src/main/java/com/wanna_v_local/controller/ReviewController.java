1142680a7e413d1fa3da2ef8c1d55e8836d9d605
devyumi <yyumiwing@naver.com>
2024-11-20 01:08:47 +0900

    feat: 관리자 리뷰 관리 - 상세 조회 및 상태 변경 컨트롤러 추가

--- src/main/java/com/wanna_v_local/controller/ReviewController.java
+++ src/main/java/com/wanna_v_local/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.wanna_v_local.controller;
 
 import com.wanna_v_local.dto.request.ReviewRequestDTO;
+import com.wanna_v_local.dto.request.ReviewUpdateStatusDTO;
 import com.wanna_v_local.service.OCRService;
 import com.wanna_v_local.service.ReviewService;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,6 +33,33 @@ public class ReviewController {
     public String getReviews(ReviewRequestDTO reviewRequestDTO, Model model) {
         model.addAttribute("reviews", reviewService.findReviews(reviewRequestDTO));
         return "admin/admin-review";
+    }
+
+    @GetMapping("admin-reviews/{id}")
+    public String getReview(@PathVariable Long id, Model model) {
+        model.addAttribute("review", reviewService.findReview(id));
+        model.addAttribute("reviewUpdateStatusDTO", new ReviewUpdateStatusDTO());
+        return "admin/admin-review-details";
+    }
+
+    @PostMapping("admin-reviews/{id}/update-false")
+    public String updateReviewFalse(@PathVariable Long id, Model model,
+                                    @ModelAttribute @Validated ReviewUpdateStatusDTO reviewUpdateStatusDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("review", reviewService.findReview(id));
+            printErrorLog(bindingResult);
+            return "admin/admin-review-details";
+        }
+        reviewService.updateReviewActiveFalse(id, reviewUpdateStatusDTO.getNote());
+        log.info("{}반 리뷰 숨김 완료", id);
+        return "redirect:/admin-reviews";
+    }
+
+    @PostMapping("admin-reviews/{id}/update-true")
+    public String updateReviewTrue(@PathVariable Long id) {
+        reviewService.updateReviewActiveTrue(id);
+        log.info("{}번 리뷰 게시 완료", id);
+        return "redirect:/admin-reviews";
     }
 
     private static void printErrorLog(BindingResult result) {
