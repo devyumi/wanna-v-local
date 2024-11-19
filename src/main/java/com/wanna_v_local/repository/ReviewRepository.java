d294b91528d3f457f4b2c057f8929bea6f2285f6
devyumi <yyumiwing@naver.com>
2024-11-19 15:01:40 +0900

    refactor: 코드 리팩토링

--- src/main/java/com/wanna_v_local/repository/ReviewRepository.java
+++ src/main/java/com/wanna_v_local/repository/ReviewRepository.java
@@ -2,8 +2,17 @@ package com.wanna_v_local.repository;
 
 import com.wanna_v_local.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository {
+
+    @Query("SELECT r FROM Review r " +
+            "LEFT JOIN r.reviewTags rt " +
+            "WHERE r.user.id = :userId " +
+            "ORDER BY r.createdAt DESC")
+    List<Review> findAllByUserIdDesc(Long userId);
 }
