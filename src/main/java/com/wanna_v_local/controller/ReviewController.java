863281bfa4d25ce70f32b0e93d8bdfec98b2c0d7
devyumi <yyumiwing@naver.com>
2024-11-24 00:16:07 +0900

    feat: 영수증 처리 로직 컨트롤러 추가

--- src/main/java/com/wanna_v_local/controller/ReviewController.java
+++ src/main/java/com/wanna_v_local/controller/ReviewController.java
@@ -1,9 +1,14 @@
 package com.wanna_v_local.controller;
 
+import com.wanna_v_local.domain.Restaurant;
 import com.wanna_v_local.dto.request.ReviewRequestDTO;
+import com.wanna_v_local.dto.request.ReviewSaveDTO;
 import com.wanna_v_local.dto.request.ReviewUpdateStatusDTO;
+import com.wanna_v_local.dto.request.TagRequestDTO;
+import com.wanna_v_local.dto.response.OCRResponseDTO;
 import com.wanna_v_local.service.OCRService;
 import com.wanna_v_local.service.ReviewService;
+import com.wanna_v_local.service.TagService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,10 +16,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,9 +30,46 @@ public class ReviewController {
 
     private final ReviewService reviewService;
     private final OCRService ocrService;
+    private final TagService tagService;
 
     /**********************USER************************/
 
+    @GetMapping("upload-receipt")
+    public String uploadReceipt() {
+        return "review/receipt";
+    }
+
+    @PostMapping("upload-receipt")
+    public String ProcessReceipt(@RequestParam("file") MultipartFile file, Model model,
+                                 RedirectAttributes redirectAttributes) throws IOException {
+        OCRResponseDTO responseDTO = ocrService.findReceiptData(file);
+
+        //영수증 처리 불가
+        if (responseDTO.getImages().get(0).getReceipt() == null
+                || responseDTO.getImages().get(0).getReceipt().getResult() == null
+                || responseDTO.getImages().get(0).getReceipt().getResult().getStoreInfo() == null
+                || responseDTO.getImages().get(0).getReceipt().getResult().getStoreInfo().getBizNum() == null
+                || responseDTO.getImages().get(0).getReceipt().getResult().getPaymentInfo() == null) {
+            log.info("영수증 처리 불가");
+            model.addAttribute("alertMessage", "영수증이 아니거나 식당명을 확인할 수 없습니다.");
+            return "review/receipt";
+        }
+
+        OCRResponseDTO.StoreInfo storeInfo = responseDTO.getImages().get(0).getReceipt().getResult().getStoreInfo();
+        Restaurant restaurant = ocrService.findCorrectRestaurant(storeInfo);
+
+        //목록에 없는 식당 처리 불가
+        if (restaurant == null) {
+            log.info("목록에 없는 식당: {}", storeInfo.getName().getText());
+            model.addAttribute("alertMessage", "목록에 없는 식당입니다.");
+            return "review/receipt";
+        }
+
+        //정상 처리
+        redirectAttributes.addFlashAttribute("restaurant", restaurant);
+        redirectAttributes.addFlashAttribute("visitDate", ocrService.findCorrectVisitDate(responseDTO.getImages().get(0).getReceipt().getResult().getPaymentInfo().getDate().getText()));
+        return "redirect:/reviews/write";
+    }
 
     /**********************ADMIN************************/
 
@@ -60,6 +104,12 @@ public class ReviewController {
         reviewService.updateReviewActiveTrue(id);
         log.info("{}번 리뷰 게시 완료", id);
         return "redirect:/admin-reviews";
+    }
+
+    @GetMapping("reviews/tags")
+    public String getReviewTags(TagRequestDTO tagRequestDTO, Model model) {
+        model.addAttribute("tags", tagService.findTags(tagRequestDTO));
+        return "admin/admin-tag";
     }
 
     private static void printErrorLog(BindingResult result) {
