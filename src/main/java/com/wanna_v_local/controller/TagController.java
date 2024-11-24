40e45bfa3eef52c9ecc77cdca8bef9b815a39ea6
devyumi <yyumiwing@naver.com>
2024-11-24 19:37:44 +0900

    feat: 관리자 태그 관리 - 태그 추가 컨트롤러 추가

--- src/main/java/com/wanna_v_local/controller/TagController.java
+++ src/main/java/com/wanna_v_local/controller/TagController.java
@@ -26,8 +26,23 @@ public class TagController {
     @GetMapping("reviews/tags")
     public String getReviewTags(TagRequestDTO tagRequestDTO, Model model) {
         model.addAttribute("tags", tagService.findTags(tagRequestDTO));
+        model.addAttribute("tagSaveDTO", new TagSaveDTO());
         model.addAttribute("tagUpdateDTO", new TagSaveDTO());
         return "admin/admin-tag";
+    }
+
+    @PostMapping("reviews/tags/write")
+    public String saveTag(@ModelAttribute @Validated TagSaveDTO tagSaveDTO, BindingResult bindingResult,
+                          RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            printErrorLog(bindingResult);
+            redirectAttributes.addFlashAttribute("alertMessage", bindingResult.getFieldErrors().get(0).getDefaultMessage());
+            return "redirect:/reviews/tags";
+        }
+        tagService.saveTag(tagSaveDTO);
+        log.info("태그 추가 완료");
+        redirectAttributes.addFlashAttribute("alertMessage", "추가 되었습니다.");
+        return "redirect:/reviews/tags";
     }
 
     @PostMapping("reviews/tags/{id}/update")
